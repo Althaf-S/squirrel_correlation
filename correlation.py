66b3288c5a51f35ad2e899e0b3f348939427ddc3
Althaf S <althaf.sabsara@gmail.com>
2023-11-12 22:36:38 +0530

    Output have been obtained

--- correlation.py
+++ correlation.py
@@ -1,36 +1,50 @@
 import json
 import math
 
+
 def load_journal(j_file = 'journal.json'):
    with open(j_file, 'r') as journal:
       journal_data = json.load(journal)
-   return journal_data
+   return list(journal_data)
+   j_file.close()
 
-def compute_phi():
-    data = list(load_journal())
-    #print(data)
-    events_done = []
-    n00,n01,n10,n11 = 0,0,0,0
-    n1a,n0a,na1,na0 = 0,0,0,0
+def compute_phi(data,event):
+    n11 = 0
+    n00 = 0
+    n10 = 0
+    n01 = 0
     for i in data:
-      for event in i['events']:
-        events_done.append(event)
-      is_event = i['events']
-      is_squirrel = i['squirrel']
-      for j in events_done:
-         if j in is_event and is_squirrel:
+        is_squirrel = i['squirrel']
+        is_event = event in i['events']
+        if is_squirrel and is_event:
             n11 += 1
-         elif j in is_event and not is_squirrel:
+        elif not is_squirrel and not is_event:
+            n00 += 1
+        elif not is_squirrel and is_event:
             n10 += 1
-         elif j not in is_event and is_squirrel:
+        elif is_squirrel and not is_event:
             n01 += 1
-         elif j not in is_event and not is_squirrel:
-            n00 =+ 1
-         
 
-#print(load_journal(),)
-compute_phi()
+    phi = (n11 * n00 - n10 * n01) / ((n11 + n10) * (n01 + n00) * (n11 + n01) * (n10 + n00)) ** 0.5
+    return phi
     
     
-    
-       
+def main():
+  data = load_journal()
+  events_done = []
+  for i in data:
+    for j in i['events']:
+      events_done.append(j)
+  correlation = {}
+  for i in events_done:
+    result = compute_phi(data,i)
+    correlation[i]=result
+    print(correlation)
+  print("most_correlated_event =   "+max(correlation,key=correlation.get))
+
+  print("min_correlated_event  =   "+min(correlation,key=correlation.get))
+
+if __name__ == "__main__":
+  main()
+
+
